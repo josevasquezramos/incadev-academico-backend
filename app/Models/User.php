6eb9abe11e14c558a46251c94e2d8cc567ed7383
josevasquezramos <jmvr16092003@gmail.com>
2025-11-03 21:08:42 -0500

    chore: initial setup

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use IncadevUns\CoreDomain\Traits\HasIncadevCore;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,
+        HasRoles, HasApiTokens, HasIncadevCore;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +25,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'dni',
+        'fullname',
+        'avatar',
+        'phone',
     ];
 
     /**
